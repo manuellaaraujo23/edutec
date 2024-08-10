2530fbd967ab31e1a036f52b72a8ff6fa5c3b1bc
manuella araujo <silva.manuella@eaportal.org>
2024-08-10 19:34:59 +0000

    adicionado mais paginas de HTML

---
+++ quiz/questions.js
@@ -0,0 +1,90 @@
+{
+    "quizzes"; [
+        {
+            "title": "Facil",
+            "icon": "../assets/images/nivel-facil.png",
+            "questions": [
+                {
+                    question  : "O que é uma cadeia alimentar?",
+                    answers: [
+                        { Option: "Um tipo de planta", correct: false},
+                        { Option: "A sequência de organismos que se alimentam uns dos outros", correct: true},
+                        { Option: "Um ciclo de água", correct: false},
+                    ],
+                },
+                {
+                    question: "Quem está no topo da cadeia alimentar?",
+                    answers: [
+                        { Option: "Produtores", correct: false},
+                        { Option: "Consumidores primarios", correct: false},
+                        { Option: "Predadores", correct: true},
+                    ],
+                },
+                {
+                    question: "Quem são os produtores em uma cadeia alimentar?",
+                    answers: [
+                        { Option: "Animais carnívoros", correct: false},
+                        { Option: "Plantas e algas", correct: true},
+                        { Option: "Decompositores", correct: false},
+                    ],
+                },
+                {
+                    question: "Qual o papel dos consumidores secundários em uma cadeia alimentar?",
+                    answers: [
+                        { Option: "Comer plantas", correct: false},
+                        { Option: "Comer consumidores primários", correct: true},
+                        { Option: "Produzir energia", correct: false},
+                    ],
+                },
+                {
+                    question: "O que é uma teia alimentar?",
+                    answers: [
+                        { Option: "Um único caminho de alimentação", correct: false},
+                        { Option: "Uma rede complexa de cadeias alimentares interconectadas", correct: true},
+                        { Option: "Um tipo de solo", correct: false},
+                    ],
+                },
+                {
+                    question: "Qual dos seguintes é um exemplo de consumidor primário?",
+                    answers: [
+                        { Option: "Leão", correct: false},
+                        { Option: "Vaca", correct: true},
+                        { Option: "Águia", correct: false},
+                    ],
+                },
+                {
+                    question: "O que são decompositores?",
+                    answers: [
+                        { Option: "Organismos que quebram matéria orgânica morta", correct: true},
+                        { Option: "Animais que caçam outros animais", correct: false},
+                        { Option: "Plantas que fazem fotossíntese", correct: false},
+                    ],
+                },
+                {
+                    question: "Em uma teia alimentar, quem come um herbívoro?",
+                    answers: [
+                        { Option: "Um produtor", correct: false},
+                        { Option: "Um carnívoro", correct: true},
+                        { Option: "Um decompositor", correct: false},
+                    ],
+                },
+                {
+                    question: "O que acontece se um predador em uma cadeia alimentar for removido?",
+                    answers: [
+                        { Option: "Nada muda", correct: false},
+                        { Option: "A população dos consumidores primários pode aumentar", correct: true},
+                        { Option: "A cadeia alimentar fica mais curta", correct: false},
+                    ],
+                },
+                {
+                    question: "Qual dos seguintes não faz parte de uma cadeia alimentar?",
+                    answers: [
+                        { Option: "Sol", correct: true},
+                        { Option: "Planta", correct: false},
+                        { Option: "Predador", correct: false},
+                    ],
+                }
+            ]    
+        }
+    ]
+}
